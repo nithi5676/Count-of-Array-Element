b7f90fa791a21fa7deaedded3117823e38e900b4
nithi5676 <nithish_kumar@outlook.in>
2023-07-12 14:56:49 +0530

    initial commit done

---
+++ CountOfArray.java
@@ -0,0 +1,37 @@
+package countOfArray;
+
+import java.util.Scanner;
+
+class CountOfArray {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int size = scanner.nextInt();
+        int array[] = new int[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = scanner.nextInt();
+        }
+        scanner.close();
+        CountOfArray countOfArray = new CountOfArray();
+        countOfArray.Count(array, size);
+
+    }
+
+    public void Count(int array[], int size) {
+        int max = Integer.MIN_VALUE;
+        // System.out.println(max);
+        int counter = 0;
+        for (int i = 0; i < size; i++) {
+            if (array[i] > max) {
+                max = array[i];
+            }
+        }
+        for (int i = 0; i < size; i++) {
+            if (array[i] < max) {
+                counter++;
+            }
+        }
+        System.out.println(counter);
+
+    }
+}
